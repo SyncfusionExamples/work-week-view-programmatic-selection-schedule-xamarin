25b5b5f61eed90f4d19bbfaa5990255c8aa276a5
sakithya <sakithya.chidambaram@syncfusion.com>
2021-02-12 15:00:48 +0530

    Work week slection

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulePageBehavior.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulePageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.FindByName<SfSchedule>("schedule");
+
+            // Setting a date and time to select
+            schedule.SelectedDate = new DateTime(2017, 10, 04, 10, 0, 0);
+
+            // Setting null value to deselect
+            //schedule.SelectedDate = null;
+        }
+    }
+}
+
+       
